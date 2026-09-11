29790ced8f13fd546aee602236ef5e56c63a348c
Karan Manikani <73251461+Karan-Manikani@users.noreply.github.com>
2026-09-11 16:01:27 -0700

    Add: k-closest-points-to-origin - submission-0

---
+++ Data Structures & Algorithms/k-closest-points-to-origin/submission-0.ts
@@ -0,0 +1,44 @@
+interface QueueNode {
+    dist: number,
+    coordinates: [number, number]
+}
+
+class Solution {
+    /**
+     * @param {number[][]} points
+     * @param {number} k
+     * @return {number[][]}
+     */
+    kClosest(points: number[][], k: number): number[][] {
+        const distances: QueueNode[] = points.map((point) => {
+            const dist =  this.euclideanDistance(point[0], point[1]);
+            return { dist, coordinates: point } as QueueNode;
+        })
+        const pq = new PriorityQueue((a, b) => b.dist - a.dist);
+        for (const distance of distances) {
+            pq.enqueue(distance);
+            if (pq.size() > k) {
+                pq.dequeue();
+            }
+        }
+        const output: [number, number][] = [];
+        while (pq.size() !== 0) {
+            const item = pq.dequeue();
+            output.push(item.coordinates);
+        }
+
+        return output;
+    }
+
+    euclideanDistance(x: number, y: number): number {
+        return Math.sqrt((x ** 2) + (y ** 2));
+    }
+}
+
+/**
+ * Min heap of size k
+ * For every point:
+ * - calculate the distance from origin
+ * - enqueue: { dist: X, point: [a, b] } ordered by dist.
+ * - Once everything is enqueued, iterate over all elements to get the reislt.
+ */
